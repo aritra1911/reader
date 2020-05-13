a6d588f53c96d5f9767b896d1ffd1905a3c8c91e
Aritra Sarkar <aritra1911@yahoo.com>
2020-05-14 02:33:24 +0530

    Add File Listing in Index

--- flask/src/views.py
+++ flask/src/views.py
@@ -1,15 +1,21 @@
 from . import app
 from .journal import JournalEntry
 from flask import render_template
-from os import environ, sep
+from os import environ, sep, listdir, path
+
+PATH=environ['JOURNAL_DIR']
 
 @app.route("/")
 def index():
-    return "HelloWorld!"
+    return render_template("index.html", files=[
+        f
+        for f in listdir(PATH)
+        if path.isfile(path.join(PATH, f)) and f.endswith('.jrl')
+    ])
 
 @app.route("/<filename>")
 def render_journal(filename):
-    entry = JournalEntry(environ['JOURNAL_PATH'] + sep + filename)
+    entry = JournalEntry(PATH + sep + filename)
     return render_template("journal.html",
         title=entry.get_title(),
         body=entry.get_paragraphs(),
