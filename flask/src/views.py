b2790a4e7cde924d8e8e01d8d4c9d9c8a0e29927
Aritra Sarkar <aritra1911@yahoo.com>
2020-05-16 02:07:20 +0530

    Add titles next to file links, also sort files according to date

--- flask/src/views.py
+++ flask/src/views.py
@@ -4,28 +4,47 @@ from .helpers import cycle
 from flask import render_template
 from os import environ, sep, listdir, path
 
-PATH=environ['JOURNAL_DIR']
+# TODO, Move these to `constants.py`
+PATH = environ['JOURNAL_DIR']
+DECRYPT_FUNC = lambda msg: ''.join([
+    chr((ord(c) - (65 if not c.islower() else 97) - (ord(s) - 97)) % 26
+    + (65 if not c.islower() else 97)) if c.isalpha() else c
+    for c, s in zip(msg, cycle(msg, 'lollipop'))
+])
 
 @app.route("/")
 def index():
-    return render_template("index.html", files=[
+    files = [
         f
         for f in listdir(PATH)
         if path.isfile(path.join(PATH, f)) and f.endswith('.jrl')
-    ])
+    ]
+    files.sort()
+
+    entry = JournalEntry()
+    journals = dict()  # journals['filename': 'title']
+    for file in files:
+        entry.set_filename(PATH + sep + file)
+
+        # You might ask why on earth I'm not optimising read_file() to just get
+        # the title in this case. Well, I ran a few tests and recorded the times
+        # they took to load the file_content. Apparently there's not much
+        # advantage in doing that. Also somehow reading the file line by line
+        # wasn't actually quicker than dumping it all at once into a variable.
+        # I'm also using a single object here in order to cut down on some memory
+        # usage.
+        entry.read_file(decrypt=DECRYPT_FUNC)
+        entry.parse()
+        journals.update({file: entry.get_title()})
+
+    return render_template('index.html', journals_menu=journals)
 
 @app.route("/<filename>")
 def render_journal(filename):
     entry = JournalEntry()
     entry.set_filename(PATH + sep + filename)
 
-    # Decrypt file
-    entry.read_file(decrypt=lambda msg: ''.join([
-        chr((ord(c) - (65 if not c.islower() else 97) - (ord(s) - 97)) % 26
-        + (65 if not c.islower() else 97)) if c.isalpha() else c
-        for c, s in zip(msg, cycle(msg, 'lollipop'))
-    ]))
-
+    entry.read_file(decrypt=DECRYPT_FUNC)
     entry.parse()
 
     return render_template("journal.html",
